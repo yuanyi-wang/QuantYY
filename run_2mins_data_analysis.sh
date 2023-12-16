b1f4212a3747fe1d99295ad73f7997904f6ab3ff
Yuanyi Wang <yuanyi.wang@outlook.com>
2023-12-16 11:12:29 +0000

    add loguru

--- run_2mins_data_analysis.sh
+++ run_2mins_data_analysis.sh
@@ -8,4 +8,6 @@ cd $SCRIPT_PATH
 
 source .venv/bin/activate
 
+LOG_FILE_NAME=$(date +'%Y-%m-%d_%H%M')
+
 python src/2mins_data_analysis.py
--- src/2mins_data_analysis.py
+++ src/2mins_data_analysis.py
@@ -1,18 +1,15 @@
-from datetime import date, datetime
-import os
+import os, time
 import pickle
-import pytz
 
 import akshare as ak
 from loguru import logger
 
 import common.supports as supports
 
-def get_data_file_name():
-    asia_shanghai = pytz.timezone('Asia/Shanghai')
-    now = datetime.now(asia_shanghai)
 
-    formatted_time = now.strftime('%H%M')
+@logger.catch()
+def get_data_file_name():
+    formatted_time = supports.get_now().strftime('%H%M')
     file_name = f"stock_zh_{formatted_time}.pkl"
     data_folder_name = supports.get_today_data_folder()
     if not os.path.exists(data_folder_name) :
@@ -21,14 +18,19 @@ def get_data_file_name():
     data_file = os.path.join(data_folder_name, file_name)
     return data_file
 
+
+@logger.catch()
 def download_and_save_zh_stock(data_file):
     stock_zh = ak.stock_zh_a_spot_em()
     with open(data_file, 'wb') as file:
         pickle.dump(stock_zh, file)
-        print(f'Object successfully saved to "{data_file}"')
+        logger.info(f'Object successfully saved to "{data_file}"')
 
 if __name__ == '__main__':
+    supports.init_app("2mins_data_analysis")
+    
     logger.info("2 minutes data analysis start")
+    
     if supports.today_market_open():
         data_file = get_data_file_name()
         download_and_save_zh_stock(data_file)
